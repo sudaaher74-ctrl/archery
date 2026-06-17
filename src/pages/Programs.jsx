5e61a111eace2dfe5f6e4e618ff1cbe15e7c969a
sudaaher74-ctrl <sudaaher74@gmail.com>
2026-06-17 15:15:34 +0530

    update

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SEO from '../components/seo/SEO';
 import InteractiveTarget from '../components/target/InteractiveTarget';
+import AnnualCompetition from '../components/events/AnnualCompetition';
 import Footer from '../components/Footer';
 
 const Programs = () => {
@@ -13,6 +14,7 @@ const Programs = () => {
       />
       <div style={{ paddingTop: '80px' }}>
         <InteractiveTarget />
+        <AnnualCompetition />
         <Footer />
       </div>
     </>
